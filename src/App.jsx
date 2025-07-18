5cb16d1376059496db164dd5da542d653c7200b7
Lianzin04 <xurumelasensata@gmail.com>
2025-07-18 14:02:54 +0000

    na aula eu vejo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import "./styles/theme.css";
+import "./styles/theme.css"
 import "./styles/global.css";
 import { ProductList } from "./components2/ProductList";
 
 
 export default function App() {
+  
+  const [cart, setCart] = useState([]);
+
+  function addToCart(product) {
+    setCart((prevCart) => [...prevCart, product]);
+  }
+
   return (
     //React Fragment
     <>
-      <ProductList/>
+      <Header cart={cart} />
+      <ProductList addToCart={addToCart} />
     </>
   );
 }
